be344e86a5cafb58487c33d4635634a92b38b3d6
adamd <adam.d.hinton@gmail.com>
2024-04-26 22:51:11 -0400

    running threads concurrently

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package dev.lpa;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Main thread running");
+        // interesting this can be called from main thread
+        try {
+            System.out.println("Main thread paused for one second");
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
+        System.out.println("Main thread continues here");
+
+
+        Thread thread = new Thread(() ->{
+            String tName = Thread.currentThread().getName();
+            Thread.
+           System.out.println(tName + "should take 10 dots to run");
+        });
     }
 }
